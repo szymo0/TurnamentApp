0fb9f0126849ba9bb5f40c5d111d1b1cd5536ac7
Szymon Szczpeański <szymon.szczepanski@gmail.com>
2018-02-06 13:08:34 +0100

    add  new dll - domain one

---
+++ TournamentEngine.Domain/PageContent/News.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TournamentEngine.Domain.PageContent
+{
+    public class News
+    {
+        public string Title { get; private set; }
+        public string Content { get; private set; }
+        public DateTime Created { get; private set; }
+        public DateTime HighlightedTo { get; private set; }
+        public string CreatedBy { get; private set; }
+    }
+}
